13ad695ff710222ea026a665682c2280d9df150d
Poof <poof@coding4kicks.com>
2013-07-08 20:05:38 +0000

    Added file read to web.js and index.html.

--- web.js
+++ web.js
@@ -2,6 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
